530f22a95d679a0808116d3f7224276f89a10969
TurinShayla <shayla.turin@gmail.com>
2022-11-23 20:33:52 -0500

    I added color changes, brush size changes, and eraser in a custom view

---
+++ CISC682Doodle1/app/src/main/java/com/example/cisc682doodle1/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.cisc682doodle1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import java.util.UUID;
+import android.provider.MediaStore;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private DrawingView drawView;
+    private ImageButton currPaint, drawBtn, eraseBtn, newBtn, saveBtn;
+    private float smallBrush, mediumBrush, largeBrush;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawView = findViewById(R.id.drawing);
+        drawView.setBrushSize(mediumBrush);
+
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+
+        smallBrush = getResources().getInteger(R.integer.small_size);
+        mediumBrush = getResources().getInteger(R.integer.medium_size);
+        largeBrush = getResources().getInteger(R.integer.large_size);
+
+        drawBtn = (ImageButton)findViewById(R.id.draw_btn);
+        drawBtn.setOnClickListener(this);
+
+        eraseBtn = (ImageButton)findViewById(R.id.erase_btn);
+        eraseBtn.setOnClickListener(this);
+
+    }
+
+    public void paintClicked(View view){
+        //use chosen color
+       // drawView.setErase(false);
+     //   drawView.setBrushSize(drawView.getLastBrushSize());
+
+        if(view!=currPaint){
+        //update color
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+            //reflect in the UI
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint = (ImageButton)view;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        //respond to clicks
+        if(view.getId()==R.id.draw_btn){
+            //draw button clicked
+
+            final Dialog brushDialog = new Dialog(this);
+            brushDialog.setTitle("Brush size:");
+            brushDialog.setContentView(R.layout.brush_chooser);
+
+
+
+
+            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+            smallBtn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(smallBrush);
+                    drawView.setLastBrushSize(smallBrush);
+                    drawView.setErase(false);
+                    brushDialog.dismiss();
+                }
+            });
+
+            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+            mediumBtn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(mediumBrush);
+                    drawView.setLastBrushSize(mediumBrush);
+                    drawView.setErase(false);
+                    brushDialog.dismiss();
+                }
+            });
+
+            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+            largeBtn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    drawView.setBrushSize(largeBrush);
+                    drawView.setLastBrushSize(largeBrush);
+                    drawView.setErase(false);
+                    brushDialog.dismiss();
+                }
+            });
+
+            brushDialog.show();
+        }
+
+        else if(view.getId()==R.id.erase_btn){
+            //switch to erase - choose size
+            drawView.setErase(true);
+        }
+    }
+}
